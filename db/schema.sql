a5ec1145a86c886a7fe84664a78405a969280467
NumiBlue <thekindlyblog@gmail.com>
2022-06-05 22:30:32 -0400

    begins to create tables

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    
+)
