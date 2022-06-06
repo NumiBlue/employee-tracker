772f6df354d841e457b3c5ba7099677419d1fc02
NumiBlue <thekindlyblog@gmail.com>
2022-06-05 22:43:06 -0400

    adds table employees

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL);
+    department_name VARCHAR(30) NOT NULL
+    );
 
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -8,5 +9,18 @@ CREATE TABLE roles (
     salary DECIMAL NOT NULL,
     departments_id INTEGER NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department (id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id) REFERENCES roles(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
     ON DELETE SET NULL
 );
