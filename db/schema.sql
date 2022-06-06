0441f94ce1c143bef71f09beb697d9ac3780204c
NumiBlue <thekindlyblog@gmail.com>
2022-06-05 22:38:33 -0400

    makes table for roles

--- db/schema.sql
+++ db/schema.sql
@@ -4,5 +4,9 @@ CREATE TABLE department (
 
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    
-)
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    departments_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department (id)
+    ON DELETE SET NULL
+);
