035d50a4f9a151a5c3fcf6984a7791434e7386a0
NumiBlue <thekindlyblog@gmail.com>
2022-06-06 19:55:30 -0400

    creates roles

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT -- Insert rows into table 'TableName'
+INSERT INTO department (department_name)
+VALUES
+ ("Barista"),
+ ("Manager"),
+ ("Waitress"),
+ ("Host");
+
+ INSERT INTO roles (title, salary, department_id)
+VALUES
+("Barista", 25000, 1),
+("Lead Manager", 35000, 2),
+("Day Shift Leader", 26500, 3),
+("Night Shift Leader", 27000, 3),
+("Host", 13000, 4),
+("Intern". 9000, 4),
+
+
+
+),
+( -- second row: values for the columns in the list above
+ Column1_Value, Column2_Value, Column3_Value
+)
+-- add more rows here
+GO
